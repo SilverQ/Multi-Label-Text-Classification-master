07af3a28522f1248cd99b2f294ece782ad00e170
hdh <scor9947@hanmail.net>
2019-08-28 11:00:57 +0900

    3'rd commit

--- CNN/train_cnn_ver2.py
+++ CNN/train_cnn_ver2.py
@@ -2,6 +2,7 @@
 import sys
 import tensorflow as tf
 import os
+import pickle
 import time
 import numpy as np
 import json
@@ -13,19 +14,49 @@ from tensorboard.plugins import projector
 # from text_cnn import TextCNN
 # tf.enable_eager_execution()
 
+data_path = '../data/'
+one_hot_file = os.path.join('../data/', 'tokenizer.pickle')
+BATCH_SIZE = 10
+EPOCH_SIZE = 2
+
+tf.keras.backend.clear_session()
+
+# raw_path = '../data/Raw_data'
+# file_list = ['Section_Label.txt', 'Section_Title.txt']
+
+
+def write_file(input_data, file_path):
+    # file_path = os.path.join('../data/', file_name)
+    with open(file_path, 'wb') as write_file1:
+        pickle.dump(input_data, write_file1)
+        print('Writing File {} finished'.format(file_path))
+
 
 def train_section_title():
-    # raw_path = '../data/Raw_data'
-    # file_list = ['Section_Label.txt', 'Section_Title.txt']
 
+    # Check One Hot
     with open(os.path.join('../data', 'Section_Title.txt'), encoding='utf-8') as data_file:
         samples = [json.loads(line) for line in data_file]
-    # print(len(targets))     # 9362172
-    # print(len(samples))     # 9362172
+        if os.path.isfile(one_hot_file):
+            print('Loading Saved Tokenizer')
+            with open(one_hot_file, 'rb') as handle:
+                tokenizer = pickle.load(handle)
+        else:
+            # print(len(targets))     # 9362172
+            # print(len(samples))     # 9362172
+            tokenizer = preprocessing.text.Tokenizer()
+            # tokenizer = preprocessing.text.Tokenizer(num_words=None, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
+            #                                          lower=True, split=' ', char_level=False,
+            #                                          oov_token=None, document_count=0)
 
-    tokenizer = preprocessing.text.Tokenizer()
-    tokenizer.fit_on_texts(samples)  # 토크나이저 학습은 전체 데이터를 대상으로!!
-    # 그러면 실 서비스 환경에서는 신조어들이 막 튀어나올텐데, 그때는 에러나나?
+            tokenizer.fit_on_texts(samples)  # 토크나이저 학습은 전체 데이터를 대상으로!!
+            # 그러면 실 서비스 환경에서는 신조어들이 막 튀어나올텐데, 그때는 에러나나?
+            # 참고 : https://subinium.github.io/Keras-6-1/
+            with open(one_hot_file, 'wb') as handle:
+                pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    word_index = tokenizer.word_index       # 계산된 단어 인덱스
+    # print('Found %s unique tokens.' % len(word_index))    # Found 252430 unique tokens.
 
     sequences = tokenizer.texts_to_sequences(samples)
     sequences = preprocessing.sequence.pad_sequences(sequences, maxlen=15, padding='post')
@@ -42,8 +73,23 @@ def train_section_title():
 
     # [기본 코드]
     dataset = tf.data.Dataset.from_tensor_slices((sequences, targets))  # 튜플로 감싸서 넣으면 tf.data가 알아서 잘라서 쓴다
-    iterator = dataset.make_one_shot_iterator()  # 없어도 됨, If using `tf.estimator`, return the `Dataset` object directly from your input function
+    # dataset = dataset.shuffle(buffer_size=10000).repeat(EPOCH_SIZE).batch(BATCH_SIZE)
+    iterator = dataset.make_one_shot_iterator()  # 없어도 됨,
+    # If using `tf.estimator`, return the `Dataset` object directly from your input function
     next_data = iterator.get_next()
+
+    def view_sample_datas():
+        # tf.keras.clear_session()
+        tf.keras.backend.clear_session()
+        with tf.Session() as sess:
+            seq, lab = next_data
+            print(sess.run([seq, lab]))
+            """
+            [array([   90,    15,  6913,    62, 10744,   282,     1,    15,  7230,
+                    1832,   267,     1,    19,   373,    13], dtype=int32), array([b'C'], dtype=object)]
+            """
+
+    view_sample_datas()
 
     # with tf.Session() as sess:
     #     while True:
@@ -52,39 +98,6 @@ def train_section_title():
     #             print(sess.run([seq, lab]))
     #         except:
     #             break  # 갯수가 끝나면 에러뜨면서 종료
-
-    #
-    # tokenizer = preprocessing.text.Tokenizer(num_words=None, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True,
-    #                                          split=' ', char_level=False, oov_token=None, document_count=0)
-    # # tokenizer = preprocessing.text.Tokenizer()
-    # dataset = tf.data.TextLineDataset(file_list)
-    # # tokenizer.fit_on_texts(dataset)  # 토크나이저 학습은 전체 데이터를 대상으로!!
-    # # 그러면 실 서비스 환경에서는 신조어들이 막 튀어나올텐데, 그때는 에러나나?
-    #
-    # BATCH_SIZE = 10
-    # EPOCH_SIZE = 2
-    #
-    # # dataset = tf.data.TextLineDataset(file_list).map(parse_fn)
-    # dataset = dataset.shuffle(buffer_size=10000).repeat(EPOCH_SIZE).batch(BATCH_SIZE)
-    # iterator = dataset.make_one_shot_iterator()
-    # next_data = iterator.get_next()
-    # # idkipi, pd, cpc, title, p = iterator.get_next()
-    #
-    # # dataset = dataset.shuffle(buffer_size=10000)       # 셔플을 먼저 해주고 배치를 자르자.
-    # # dataset = dataset.batch(BATCH_SIZE)
-    # # dataset = dataset.repeat(EPOCH_SIZE)
-    #
-    # with tf.Session() as sess:
-    #     tokenizer.fit_on_texts(dataset)  # 토크나이저 학습은 전체 데이터를 대상으로!!
-    #     tmp = sess.run(next_data)
-    #     print(tmp)
-    #     # print('data: ', tmp('id_kipi'), len(tmp))
-    #     # while True:
-    #     #     try:
-    #     #         # seq, lab = next_data
-    #     #         print('data: ', sess.run(next_data))
-    #     #     except:
-    #     #         break  # 갯수가 끝나면 에러뜨면서 종료
 
 
 if __name__ == '__main__':
